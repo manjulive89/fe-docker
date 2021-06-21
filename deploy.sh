f39f1ea289b174aa45470e3677d67c17edc8981e
MAnjunath CR <manjulive89@gmail.com>
2021-06-21 09:59:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-echo "Docker Container check"
-sudo docker ps -aq
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
-sudo docker rmi $(docker images -q)
-cd /home/sanjay
-rm -rf fe-docker
 git clone https://github.com/manjulive89/fe-docker.git
+cd fe-docker
 npm ci
 npm build run
 docker build -t sanjay app .
 docker run -d -p 5000:5000 --name app1.0 sanjayapp
+
